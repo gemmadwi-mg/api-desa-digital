77bdb4fa523e1c415790c69291c490ee219f7584
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2026-09-04 18:28:05 +0700

    make update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ResponseHelper;
+use App\Http\Requests\ProfileStoreRequest;
+use App\Http\Requests\ProfileUpdateRequest;
 use App\Http\Resources\ProfileResource;
 use App\Interfaces\ProfileRepositoryInterface;
 use Illuminate\Http\Request;
@@ -26,6 +28,32 @@ class ProfileController extends Controller
             }
 
             return ResponseHelper::jsonResponse(true, 'Profile Berhasil Diambil', new ProfileResource($profile), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    public function store(ProfileStoreRequest $request)
+    {
+        $request = $request->validated();
+
+        try {
+            $profile = $this->profileRepository->create($request);
+
+            return ResponseHelper::jsonResponse(true, 'Data Profile berhasil Ditambahkan', new ProfileResource($profile), 201);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    public function update(ProfileUpdateRequest $request)
+    {
+        $request = $request->validated();
+
+        try {
+            $profile = $this->profileRepository->update($request);
+
+            return ResponseHelper::jsonResponse(true, 'Data Profile berhasil Diubah', new ProfileResource($profile), 201);
         } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
         }
